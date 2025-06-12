a098ae37c8943c8077909762a5574c396aa0a11b
uladzislau sverbut <vladislavsverbut@gmail.com>
2025-06-12 23:09:09 +0200

    replace syscall package by sys/unix

--- internal/storage/file-utils.go
+++ internal/storage/file-utils.go
@@ -2,21 +2,12 @@ package storage
 
 import (
 	"os"
-	"syscall"
 
 	"golang.org/x/sys/unix"
 )
 
 func increaseFileSize(file *os.File, allocatedFileSize int64) error {
-	fileStat, err := file.Stat()
-
-	if err != nil {
-		return err
-	}
-
-	newFileSize := fileStat.Size() + allocatedFileSize
-
-	err = unix.FcntlFstore(file.Fd(), unix.F_ALLOCATECONTIG, &unix.Fstore_t{
+	err := unix.FcntlFstore(file.Fd(), unix.F_ALLOCATECONTIG, &unix.Fstore_t{
 		Flags:   unix.F_ALLOCATECONTIG,
 		Posmode: unix.F_PEOFPOSMODE,
 		Length:  allocatedFileSize,
@@ -26,9 +17,9 @@ func increaseFileSize(file *os.File, allocatedFileSize int64) error {
 		return err
 	}
 
-	return file.Truncate(newFileSize)
+	return file.Truncate(allocatedFileSize)
 }
 
 func mapFileToMemory(file *os.File, offset int64, size int) (data []byte, err error) {
-	return syscall.Mmap(int(file.Fd()), int64(offset), int(size), syscall.PROT_READ|syscall.PROT_WRITE, syscall.MAP_SHARED)
+	return unix.Mmap(int(file.Fd()), offset, size, unix.PROT_READ|unix.PROT_WRITE, unix.MAP_SHARED)
 }
