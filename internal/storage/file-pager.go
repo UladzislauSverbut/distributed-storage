7ba8d6dcde870f615c7a7d79160a9b3bb0d425e7
UladzislauSverbut <vladislavsverbut@gmail.com>
2025-07-24 23:19:45 +0200

    Feat/make pager immutable (#2)
    feat: start making file pager immutable

--- internal/storage/file-pager.go
+++ internal/storage/file-pager.go
@@ -4,66 +4,145 @@ import (
 	"encoding/binary"
 )
 
-/*
-store Format
+const HEADER_SIZE = 18 // size of node header in file bytes
 
-	| number of available pages | pointer to previous set of pages |  pointers to available pages   |
-	|          2B               |          8B                      | number of available pages * 8B |
+/*
+	Page Pool Node Format
+
+	| node stored pages | total stored pages | pointer to previous pool node | pointers to node stored pages |
+	|         2B        |         8B         |              8B               |  number of stored pages * 8B  |
 */
 
-type PageStore struct {
-	pageSize int
-	data     []byte
+type PagePool struct {
+	head    PagePointer
+	storage *FileStorage
 }
 
-func NewPageStore(pagesize int, data []byte) *PageStore {
-	return &PageStore{
-		pageSize: pagesize,
-		data:     data,
+func NewPagePool(head PagePointer, storage *FileStorage) *PagePool {
+	return &PagePool{
+		head:    head,
+		storage: storage,
 	}
 }
 
-func (store *PageStore) getNumberOfAvailablePages() uint16 {
-	return binary.LittleEndian.Uint16(store.data)
-}
+func (pool *PagePool) getPage(pageNumber int) PagePointer {
+	node := pool.storage.GetPage(pool.head)
 
-func (store *PageStore) setNumberOfAvailablePages(numberOfAvailablePages uint16) {
-	binary.LittleEndian.PutUint16(store.data, numberOfAvailablePages)
-}
-
-func (store *PageStore) getPrevious() PagePointer {
-	return PagePointer(binary.LittleEndian.Uint64(store.data[2:]))
-}
-
-func (store *PageStore) setPrevious(pointer PagePointer) {
-	binary.LittleEndian.PutUint64(store.data[2:], pointer)
-}
-
-func (store *PageStore) getAvailablePage() PagePointer {
-	numberOfAvailablePages := store.getNumberOfAvailablePages()
-
-	if numberOfAvailablePages == 0 {
-		panic("Cant get available page in empty pool")
+	for pool.getNodePagesCount(node) < pageNumber {
+		pageNumber -= pool.getNodePagesCount(node)
+		node = pool.storage.GetPage((pool.getPreviousNode(node)))
 	}
 
-	store.setNumberOfAvailablePages(numberOfAvailablePages - 1)
-
-	return PagePointer(binary.LittleEndian.Uint64(store.data[10+8*(numberOfAvailablePages-1):]))
+	return pool.getNodePage(node, pageNumber)
 }
 
-func (store *PageStore) addAvailablePage(pointer PagePointer) {
+func (pool *PagePool) getPagesCont() int {
+	node := pool.storage.GetPage(pool.head)
 
-	if store.isFull() {
-		panic("Cant add page pointer to full page store")
+	return pool.getNodeTotalPagesCount(node)
+}
+
+func (pool *PagePool) updatePages(reusedPagesCount int, releasedPages []PagePointer) {
+	nodePointer := pool.head
+	node := pool.storage.GetPage(nodePointer)
+	pagesCount := pool.getNodeMaxPagesCount(node)
+	reusedPages := make([]PagePointer, 0)
+
+	for pagesCount <= reusedPagesCount {
+		reusedPagesCount -= pagesCount
+
+		reusedPages = append(reusedPages, nodePointer)
+
+		nodePointer = pool.getPreviousNode(node)
+		node = pool.storage.GetPage(nodePointer)
+		pagesCount = pool.getNodePagesCount(node)
 	}
 
-	numberOfAvailablePages := store.getNumberOfAvailablePages()
+	if reusedPagesCount != 0 {
+		for pageNumber := reusedPagesCount; pageNumber < pagesCount; pageNumber++ {
+			reusedPages = append(reusedPages, pool.getNodePage(node, pageNumber))
+		}
 
-	store.setNumberOfAvailablePages(numberOfAvailablePages + 1)
+		nodePointer = pool.getPreviousNode(node)
+	}
 
-	binary.LittleEndian.PutUint64(store.data[10+8*(numberOfAvailablePages):], pointer)
+	for len(releasedPages) > 0 {
+		pages := releasedPages[:min(len(releasedPages), pagesCount)]
+		releasedPages = releasedPages[len(pages):]
+
+		newNodePointer := NULL_PAGE
+
+		if len(reusedPages) > 0 {
+			newNodePointer = releasedPages[0]
+			releasedPages = releasedPages[1:]
+		} else {
+			newNodePointer = pool.storage.allocateVirtualPage()
+		}
+
+		newNode := pool.storage.GetPage(newNodePointer)
+
+		pool.setNodePrevious(newNode, nodePointer)
+		pool.addNodePages(newNode, pages)
+
+		nodePointer = newNodePointer
+		node = newNode
+	}
+
+	for len(reusedPages) > 0 {
+		newNodePointer := reusedPages[0]
+		reusedPages = reusedPages[1:]
+		newNode := pool.storage.GetPage(newNodePointer)
+
+		pool.setNodePrevious(newNode, nodePointer)
+		pool.addNodePages(newNode, reusedPages[:pagesCount])
+
+		reusedPages = reusedPages[:pagesCount]
+
+		nodePointer = newNodePointer
+		node = newNode
+	}
 }
 
-func (store *PageStore) isFull() bool {
-	return store.pageSize < 10+int(store.getNumberOfAvailablePages()+1)*8
+func (pool *PagePool) getNodeTotalPagesCount(node []byte) int {
+	return int(binary.LittleEndian.Uint64(node[2:]))
+}
+
+func (pool *PagePool) getNodePage(node []byte, pageNumber int) PagePointer {
+	pageAddress := 10 + 8*pageNumber
+
+	return PagePointer(binary.LittleEndian.Uint64(node[pageAddress:]))
+}
+
+func (pool *PagePool) getNodePagesCount(node []byte) int {
+	return int(binary.LittleEndian.Uint16(node))
+}
+
+func (pool *PagePool) getPreviousNode(node []byte) PagePointer {
+	return binary.LittleEndian.Uint64(node[10:])
+}
+
+func (pool *PagePool) getNodeCapacity(node []byte) int {
+	return pool.getNodeMaxPagesCount(node) - pool.getNodePagesCount(node)
+}
+
+func (pool *PagePool) getNodeMaxPagesCount(node []byte) int {
+	return (len(node) - HEADER_SIZE) / 8
+}
+
+func (pool *PagePool) setNodePrevious(node []byte, previousNodePointer PagePointer) {
+	totalPagesNumber := pool.getNodePagesCount(node) + pool.getNodePagesCount((pool.storage.GetPage(previousNodePointer)))
+
+	binary.LittleEndian.PutUint64(node[2:], uint64(totalPagesNumber))
+	binary.LittleEndian.PutUint64(node[10:], uint64(previousNodePointer))
+}
+
+func (pool *PagePool) addNodePages(node []byte, pages []PagePointer) {
+	if pool.getNodeCapacity(node) < len(pages) {
+		panic("PagePool is full")
+	}
+	totalPagesCount := pool.getNodeTotalPagesCount(node)
+	pagesCount := len(pages)
+
+	binary.LittleEndian.PutUint16(node, uint16(pagesCount))
+	binary.LittleEndian.PutUint64(node[10:], uint64(totalPagesCount+pagesCount))
 }
